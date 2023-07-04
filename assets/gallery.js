3954f809340af1474652649ec9a26fb61e29897a
tomcdj71 <contact.tomc@yahoo.fr>
2023-07-04 18:31:19 +0200

    💎 STYLE: better gallery display - now mobile ready

--- assets/gallery.js
+++ assets/gallery.js
@@ -1,11 +1,52 @@
-// loop through all data-image. If an image is clicked, display it in the modal (#modal-image) and nothing more
+const images = Array.from(document.querySelectorAll('[data-image]'));
+let currentImageIndex = 0;
 
-let images = document.querySelectorAll('[data-image]');
-let modalImage = document.querySelector('#modal-image');
+function updateModalImage(src) {
+    const modalImage = document.querySelector('#modal-image');
+    modalImage.setAttribute('src', src);
+}
 
-images.forEach(image => {
-    image.addEventListener('click', function () {
-        let imageSrc = this.getAttribute('data-image');
-        modalImage.setAttribute('src', imageSrc);
+function handleImageClick(index) {
+    return function () {
+        const imageSrc = this.getAttribute('data-image');
+        updateModalImage(imageSrc);
+        currentImageIndex = index;
+    }
+}
+
+function initGallery() {
+    images.forEach((image, index) => {
+        image.addEventListener('click', handleImageClick(index));
     });
-});
+
+    document.getElementById('prev-image').addEventListener('click', () => {
+        currentImageIndex = (currentImageIndex - 1 + images.length) % images.length;
+        updateModalImage(images[currentImageIndex].getAttribute('data-image'));
+    });
+
+    document.getElementById('next-image').addEventListener('click', () => {
+        currentImageIndex = (currentImageIndex + 1) % images.length;
+        updateModalImage(images[currentImageIndex].getAttribute('data-image'));
+    });
+}
+
+function initAccordion() {
+    document.querySelectorAll("[data-accordion-target]").forEach(button => {
+        button.addEventListener('click', () => {
+            const accordionId = button.getAttribute('data-accordion-target');
+            const accordionBody = document.querySelector(accordionId);
+
+            const isOpen = button.getAttribute('aria-expanded') === 'true';
+            button.setAttribute('aria-expanded', !isOpen);
+
+            if (isOpen) {
+                accordionBody.classList.add('hidden');
+            } else {
+                accordionBody.classList.remove('hidden');
+            }
+        });
+    });
+}
+
+initGallery();
+initAccordion();
