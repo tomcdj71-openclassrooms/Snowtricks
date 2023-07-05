5a5a14f47b788add3f11134524f4ca3980e6167d
tomcdj71 <contact.tomc@yahoo.fr>
2023-07-05 23:06:02 +0200

    Refactor JS scripts (#21)
    * 📦 DEPENDENCIES: update dependencies
    
    💎 STYLE: minor adjustment to avatar button on navbar
    
    * 🔨 REFACTOR: js script (codacy warnings)
    
    * 🔨 REFACTOR: js scripts
    
    * 🔨 REFACTOR: js scripts (codacy)

--- assets/gallery.js
+++ assets/gallery.js
@@ -1,56 +1,34 @@
-(function () {
+document.addEventListener('DOMContentLoaded', () => {
+    setupGallery();
+    setupAccordion();
+});
+
+function setupGallery() {
     const images = Array.from(document.querySelectorAll('[data-image]'));
     let currentImageIndex = 0;
-
-    function updateModalImage(src) {
-        const modalImage = document.querySelector('#modal-image');
-        modalImage.setAttribute('src', src);
-    }
-
-    function handleImageClick(index) {
-        return function () {
-            const imageSrc = this.getAttribute('data-image');
-            updateModalImage(imageSrc);
+    const modalImage = document.querySelector('#modal-image');
+    images.forEach((image, index) => {
+        image.addEventListener('click', () => {
+            const imageSrc = image.getAttribute('data-image');
+            modalImage.setAttribute('src', imageSrc);
             currentImageIndex = index;
-        }
-    }
-
-    function handleNavClick(offset) {
-        return function () {
-            currentImageIndex = (currentImageIndex + offset + images.length) % images.length;
-            updateModalImage(images[currentImageIndex].getAttribute('data-image'));
-        }
-    }
-
-    function handleAccordionClick(button, accordionBody) {
-        const isOpen = button.getAttribute('aria-expanded') === 'true';
-        button.setAttribute('aria-expanded', !isOpen);
-
-        if (isOpen) {
-            accordionBody.classList.add('hidden');
-        } else {
-            accordionBody.classList.remove('hidden');
-        }
-    }
-
-    function initGallery() {
-        images.forEach((image, index) => {
-            image.addEventListener('click', handleImageClick(index));
         });
+    });
+    const handleNavClick = (offset) => {
+        currentImageIndex = (currentImageIndex + offset + images.length) % images.length;
+        modalImage.setAttribute('src', images[currentImageIndex].getAttribute('data-image'));
+    };
+    document.getElementById('prev-image').addEventListener('click', () => handleNavClick(-1));
+    document.getElementById('next-image').addEventListener('click', () => handleNavClick(1));
+}
 
-        document.getElementById('prev-image').addEventListener('click', handleNavClick(-1));
-        document.getElementById('next-image').addEventListener('click', handleNavClick(1));
-    }
-
-    function initAccordion() {
-        document.querySelectorAll("[data-accordion-target]").forEach(button => {
-            const accordionId = button.getAttribute('data-accordion-target');
-            const accordionBody = document.querySelector(accordionId);
-
-            button.addEventListener('click', () => handleAccordionClick(button, accordionBody));
+function setupAccordion() {
+    document.querySelectorAll('[data-accordion-target]').forEach(button => {
+        const accordionBody = document.querySelector(button.getAttribute('data-accordion-target'));
+        button.addEventListener('click', () => {
+            const isOpen = button.getAttribute('aria-expanded') === 'true';
+            button.setAttribute('aria-expanded', !isOpen);
+            accordionBody.classList.toggle('hidden', isOpen);
         });
-    }
-
-    initGallery();
-    initAccordion();
-})();
+    });
+}
--- assets/imageUploader.js
+++ assets/imageUploader.js
@@ -1,70 +1,55 @@
-let links = document.querySelectorAll('[data-delete]');
-links.forEach(link => {
-    link.addEventListener('click', handleDeleteClick.bind(link));
+window.addEventListener('DOMContentLoaded', () => {
+    setupDeleteActions();
+    setupVideos();
 });
 
-function handleDeleteClick(e) {
-    e.preventDefault();
-    let confirmationMessage = this.dataset.confirm;
-    if (confirm(confirmationMessage)) {
-        deleteItem(this);
-    }
+function setupDeleteActions() {
+    document.querySelectorAll('[data-delete]').forEach(item => {
+        item.addEventListener('click', (event) => {
+            event.preventDefault();
+            const confirmationMessage = item.dataset.confirm;
+            if (!confirm(confirmationMessage)) return;
+            fetch(item.getAttribute('href'), {
+                method: 'DELETE',
+                headers: {
+                    'X-Requested-With': 'XMLHttpRequest',
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({ '_csrf': item.dataset.token })
+            })
+                .then(response => response.json())
+                .then(data => {
+                    if (data.success) {
+                        item.parentElement.remove();
+                    } else {
+                        alert(data.error);
+                    }
+                })
+                .catch(console.error);
+        });
+    });
 }
 
-function deleteItem(item) {
-    fetch(item.getAttribute('href'), {
-        method: 'DELETE',
-        headers: {
-            'X-Requested-With': 'XMLHttpRequest',
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({ '_csrf': item.dataset.token })
-    })
-        .then(response => response.json())
-        .then(data => {
-            if (data.success) {
-                item.parentElement.remove();
-            } else {
-                alert(data.error);
-            }
-        }).catch(error => console.error('Error:', error));
-}
-
-window.addEventListener('DOMContentLoaded', () => {
+function setupVideos() {
     const collectionHolder = document.getElementById('videos-container');
     if (!collectionHolder) {
         return;
     }
     let index = collectionHolder.dataset.index;
-    document.getElementById('add-video').addEventListener('click', addVideo.bind(null, collectionHolder, index));
-    document.querySelectorAll('.delete-video').forEach(button => {
-        button.addEventListener('click', handleDeleteVideoClick);
+    document.getElementById('add-video').addEventListener('click', () => {
+        let prototype = collectionHolder.dataset.prototype;
+        let newForm = prototype.replace(/__name__/g, index);
+        let div = document.createElement('div');
+        div.innerHTML = newForm;
+        div.classList.add('video');
+        let deleteButton = document.createElement('button');
+        deleteButton.textContent = 'Delete';
+        deleteButton.type = 'button';
+        deleteButton.classList.add('delete-video');
+        deleteButton.addEventListener('click', () => {
+            div.remove();
+        });
+        div.appendChild(deleteButton);
+        collectionHolder.appendChild(div);
     });
-});
-
-function addVideo(collectionHolder, index) {
-    let prototype = collectionHolder.dataset.prototype;
-    let newForm = prototype.replace(/__name__/g, index);
-    let newIndex = index + 1;
-    let div = createNewVideoDiv(newForm, newIndex);
-    collectionHolder.appendChild(div);
-}
-
-function createNewVideoDiv(newForm, index) {
-    let div = document.createElement('div');
-    div.innerHTML = newForm;
-    div.classList.add('video');
-    let deleteButton = document.createElement('button');
-    deleteButton.textContent = 'Delete';
-    deleteButton.type = 'button';
-    deleteButton.classList.add('delete-video');
-    deleteButton.addEventListener('click', function () {
-        div.remove();
-    });
-    div.appendChild(deleteButton);
-    return div;
-}
-
-function handleDeleteVideoClick() {
-    this.parentElement.remove();
 }
